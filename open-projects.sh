a1f3955abc5e0e099cd40a60e89c5c51ac5cb77b
Willian <willian.agostini@gmail.com>
2020-03-12 11:11:37 -0300

    updated

---
+++ open-projects.sh
@@ -0,0 +1,3 @@
+code Tiot-app && code Tiot-server/express && code Tiot-server/mosca && code Tiot-hardware
+
+# docker run --name tiot-mongo -d mongo
---
+++ run-server.sh
@@ -0,0 +1,2 @@
+cd Tiot-server/express/ && konsole --noclose -e yarn start &&
+cd ../mosca && konsole --noclose -e yarn start
